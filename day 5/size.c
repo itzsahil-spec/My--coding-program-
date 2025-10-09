d5cdef2643fdc86ac6ca73c0c9a21645c3873b58
itzsahil-spec <mrb15954@gmail.com>
2025-10-09 14:12:54 +0530

    Add files via upload
    Signed-off-by: itzsahil-spec <mrb15954@gmail.com>

---
+++ day 5/size.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("size of int : %zu bytes\n",sizeof(int));
+    printf("size of float : %zu bytes\n",sizeof(float));
+    printf("size of char : %zu bytes\n",sizeof(char));
+    printf("size of double : %zu bytes\n",sizeof(double));
+    return 0;
+}
